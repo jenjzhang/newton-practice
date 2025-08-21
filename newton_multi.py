e851b59bd3f22ce3446946566d81c5252accf6a1
Jennifer <jenniferzhang@berkeley.edu>
2025-08-21 11:14:27 -0700

    after peer review

--- newton_multi.py
+++ newton_multi.py
@@ -7,12 +7,12 @@ from scipy.linalg import solve
 from scipy.optimize._numdiff import approx_derivative
 
 
-def grad_f(f, x, h=1e-5):
+def grad_f(f, x, h=1e-3):
     """Evaluate the gradient."""
     return approx_derivative(f, x, method='2-point')
 
 
-def hess_f(f, x, h=1e-5):
+def hess_f(f, x, h=1e-3):
     """Evaluate the Hessian."""
     grad_func = lambda v: grad_f(f, v, h=h)
     return approx_derivative(grad_func, x, method='2-point')
@@ -24,13 +24,15 @@ def update_hessian(f, x):
     
     g = grad_f(f, x)
     H = hess_f(f, x)
+
+    print("hessian: ", H)
     
     # Solve H * delta = g   instead of computing inverse explicitly
     delta = solve(H, g)
     return x - delta
 
 
-def optimize(x_0, f, tol=0.00001, h=1e-5, max_iter=1000):
+def optimize(x_0, f, tol=0.00001, h=1e-3, max_iter=1000):
     """Implements univariate Newton's method. Accepts a starting value and function to optimize, and returns a final x value.
     tol, h, and max_iter are passed as default parameters."""
 
@@ -44,6 +46,7 @@ def optimize(x_0, f, tol=0.00001, h=1e-5, max_iter=1000):
         if np.linalg.norm(x_next - x_t) < tol:
             break
         x_t = x_next
+        print("x_t: ", x_t)
         iter_count += 1
 
         if iter_count >= max_iter:
